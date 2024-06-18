9afc0b5e836388f58e67c2f0979261ee5c9d213c
spruitim <i.m.spruit@fsw.leidenuniv.nl>
2024-06-18 15:55:58 +0200

    added tasks to test

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -8,11 +8,22 @@ logfile_path = os.path.join(os.path.dirname(__file__), r'./data/tmp.csv')
 experiment_path = os.path.join(os.path.dirname(__file__), r'data')
 
 crashing_experiments = [
-    
+    "fail_testmarkers_os3_init_twice.osexp",
+    "fail_testmarkers_os3_invalid_device_address.osexp",
+    "fail_testmarkers_os3_invalid_device_tag.osexp",
+    "fail_testmarkers_os3_invalid_marker_value.osexp",
+    "fail_testmarkers_os3_invalid_object_dur.osexp",
+    "fail_testmarkers_os3_marker_duration_error.osexp",
+    "fail_testmarkers_os3_no_init_obj.osexp",
+    "fail_testmarkers_os3_no_matching_device_tags.osexp",
+    "fail_testmarkers_os3_same_value_twice.osexp",
+    "fail_testmarkers_os3_no_device_attached.osexp"
 ]
 
 normal_experiments = [
-    
+    "pass_testmarkers_os3_basic.osexp",
+    "pass_testmarkers_os3_multiple_devices.osexp",
+    "pass_testmarkers_os3_no_marker_objects.osexp"
 ]
 
 class runExperiments(unittest.TestCase):
